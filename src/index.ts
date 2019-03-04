09ca0caf39be6991fb9069e9953914f0337fbbb7
yangwenyi <yangwenyi@outlook.com>
2019-03-04 15:21:31 +0800

    implement wallet.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,69 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import { HubClient, Keyman } from 'sdag.js';
+import bip39 from 'bip39';
+
+export default class Wallet {
+
+    private address: string;
+    private keyman: Keyman;
+    private hub: HubClient;
+
+    configHub(address: string) {
+        this.address = address;
+    }
+
+    autoConfigHub(type: 'mainnet' | 'testnet') {
+        this.address = type === 'mainnet' ? 'wss://hub.sdag.io:8086' : 'ws://10.168.1.131:6615';
+    }
+
+    generateMnemonic() {
+        return bip39.generateMnemonic();
+    }
+
+    async loginWithMnemonic(mnemonic: string, password?: string) {
+        this.keyman = new Keyman(mnemonic, password);
+        this.hub = new HubClient();
+        return await this.hub.connect(this.address);
+    }
+
+    get mainAddress() {
+        return this.keyman.mainAddress;
+    }
+
+    getAddress() {
+        return this.keyman.mainAddress;
+    }
+
+    getPrivateKey() {
+        return this.keyman.mainXprivKey;
+    }
+
+    getPublicKey() {
+        return this.keyman.mainPubKey;
+    }
+
+    async getBalance() {
+        let balance = await this.hub.getBalance(this.getAddress());
+        if (balance.error) throw Error(balance.error);
+        return balance.balance;
+    }
+
+    async getHistory() {
+        return await this.hub.getTxsByAddress(this.mainAddress, 200);
+    }
+
+    async getUnit(unitId: string) {
+        return (await this.hub.getJoint(unitId)).joint;
+    }
+
+    async send(args: { amount: number, to: string, text?: string }) {
+        return await this.hub.transfer({ from: this.mainAddress, to: args.to, amount: args.amount, signEcdsaPubkey: this.keyman.mainEcdsaPubKey, }, hash => this.keyman.sign(hash));
+    }
+
+    setLocalstorage() {
+
+    }
+
+    logout() {
+        this.hub.close();
+    }
+}
